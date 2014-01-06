a3dafc08fd784a040188404d66174dfa7ade8001
Putu <nasi.be.guling@gmail.com>
2014-01-06 14:12:13 +0700

    menambahkan sql dump untuk proyek ird

---
+++ db_ird.sql
@@ -0,0 +1,5 @@
+/* currenly known affect table billing_lama.MR_MUTASIPASIEN, bilingbaru.tbl_transaksi
+tanya:
+pada tabel transaksi tanggal mana yg menjadi acuan untuk filter piutang, kalo pada tabel mutasi pasien : tanggal pulang
+
+efeknya ke tabel apa aja
